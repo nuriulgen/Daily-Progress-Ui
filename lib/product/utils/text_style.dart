c9a1a2e09733b3435977fb94580991f2891867f2
nuriulgen <bazid04ulgen@gmail.com>
2022-06-20 19:51:46 +0300

    Projenin text style için TextStyles class oluşturuldu.
    Projenin TextStyles için ProjectTextStyles class oluşturuldu.

---
+++ lib/product/utils/text_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ProjectTextStyles {
+  final TextStyle outlinedButtonStyle = const TextStyle(
+    color: Colors.white,
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+  );
+  final TextStyle textButtonStyle = const TextStyle(
+    color: Color(0xFFC1C1C1),
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+}
