78ee87cfb82e33804f9496e8a932708f29ef9206
nuriulgen <bazid04ulgen@gmail.com>
2022-06-20 17:34:43 +0300

    Image Constants sınıfı oluşturuldu.

---
+++ lib/core/constants/image_constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PngImage extends StatelessWidget {
+  const PngImage({Key? key, required this.name}) : super(key: key);
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(_nameWithPath, fit: BoxFit.cover);
+  }
+
+  String get _nameWithPath => 'assets/images/$name.png';
+}
