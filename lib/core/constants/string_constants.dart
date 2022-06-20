979f641a7ec768bf39dc1d34a1c898007f35da38
nuriulgen <bazid04ulgen@gmail.com>
2022-06-20 19:55:58 +0300

    Text için AppStringConstants class oluşturuldu.
    Projenin title,subtitle text  vb.  text tek bir yerden yönetmek için AppStringConstants class oluşturuldu.

---
+++ lib/core/constants/string_constants.dart
@@ -0,0 +1,16 @@
+class AppStringConstants {
+  static AppStringConstants? _instance;
+  static AppStringConstants? get instance {
+    _instance ??= AppStringConstants._init();
+    return _instance;
+  }
+
+  AppStringConstants._init();
+
+  final String signInTitle = 'The only productivity app you need';
+  final String signElevatedButtonText = 'Sign in with email';
+  final String signOutlinedButtonText1 = 'Google';
+  final String signOutlinedButtonText2 = 'Apple ID';
+  final String signTextButtonText =
+      'By Continuing you agree to the Terms of Conditious';
+}
